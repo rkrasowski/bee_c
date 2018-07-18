4e6785bae0739da9b9a9235c538f39183ed0cad2
Robert J Krasowski <robert@krasowski.net>
2018-07-18 13:54:43 +0000

    Register reader works, writing to file works, logic not started

---
+++ bees/speedCounter.c
@@ -0,0 +1,64 @@
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int bit;
+int a;
+int bitArray[24];
+int In=33;
+int  Out=44;
+float speedIn=55.2;
+float speedOut=66.7;
+
+
+int main (void)
+	{	
+		wiringPiSetup();
+		pinMode (2, OUTPUT);
+		pinMode (3, OUTPUT);
+		pinMode (0, INPUT);
+
+
+		digitalWrite (3,LOW);
+		delay(1);
+		digitalWrite (3,HIGH);
+		
+		bit = digitalRead(0);
+
+		for (a = 0; a<24; a=a+1)
+
+			{
+
+				printf("%d",bit);
+
+				bitArray[a] = bit;
+				printf("Printing\n");
+				printf("%d", bitArray[0]);
+
+
+				digitalWrite (2, HIGH);
+				delay(1);
+				digitalWrite (2, LOW);
+				delay(1);
+				bit = digitalRead (0);
+			
+			}
+/* Putting data into the file */
+
+		FILE *fptr;
+		fptr = fopen("file.txt","a");
+		if(fptr == NULL)
+   			{		
+     		 		printf("Error!, can not access the file, does it exist?\n");   
+    		  		exit(1);             
+   			} 
+
+		fprintf(fptr,"In=%i",In);
+		fprintf(fptr,"Out=%i",Out);
+		fprintf(fptr,"SpeedIn=%.1f",speedIn);	
+		fprintf(fptr,"SpeedOut=%.1f\n",speedOut);
+
+		fclose(fptr);
+
+		return 0;
+	}

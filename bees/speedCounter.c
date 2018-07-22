9f2b53014f4610c7b2beb821c306e2b813a53110
Robert J. Krasowski <robert@krasowski.net>
2018-07-22 15:18:49 +0000

    Working on counter function

--- bees/speedCounter.c
+++ bees/speedCounter.c
@@ -5,7 +5,7 @@
 #include <sys/time.h>
 
 /* Configuration variable*/
-int numOfChannels = 12;
+int numOfChannels = 2;
 
 /* Proram variable */
 
@@ -18,7 +18,7 @@ int in = 0;
 int out = 0;
 int inOutArray[24];
 int bitArray[24];
-long long int timeArray[24];
+long long int timeArray[24] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,};
 long long int timeDiff;
 long long int timeInArray[10000];
 long long int  timeOutArray[10000];
@@ -37,6 +37,7 @@ long long int getMicrotime();
 
 int main (void)
 	{
+		
 		wiringPiSetup();
 		pinMode (2, OUTPUT);
 		pinMode (3, OUTPUT);
@@ -66,45 +67,21 @@ int main (void)
 
 
 
-/* Just for testing */
-
-                bitArray[0] = 1;
-                inOutArray[2] = 0;
-		bitArray[3] = 0;
-
-
-
 
 /* Data analysis  */
 
-                counter(sensorNr,bitArray);
+		bitArray[0] = 1;
 
+                counter();
+		printf("#########################################\n");
+	
+		bitArray[1] = 1;
+		counter();
+		printf("#########################################\n");
 
-
-/* Gettint Unux time*/
- time_t seconds;
-
-   seconds = time(NULL);
-   printf("Seconds:  %ld\n", seconds);
-
-
-/* Putting data into the file */
-
-		FILE *fptr;
-		fptr = fopen("file.txt","a");
-		if(fptr == NULL)
-   			{
-     		 		printf("Error!, can not access the file, does it exist?\n");
-    		  		exit(1);
-   			} 
-		fprintf(fptr,"Time=%ld,",seconds);
-		fprintf(fptr,"In=%i,",In);
-		fprintf(fptr,"Out=%i,",Out);
-		fprintf(fptr,"SpeedIn=%.1f,",speedIn);	
-		fprintf(fptr,"SpeedOut=%.1f\n,",speedOut);
-
-		fclose(fptr);
-
+	
+	
+	
 
 
 		return 0;
@@ -114,21 +91,29 @@ int main (void)
 
 
 
-int counter (int sensorNrIns, int bitArray[], long long timeInArray[]) 
+int counter () 
 	{
-		 for ( loopA=0; loopA<= numOfChannels*2-1; loopA++)
+		 for ( loopA=0; loopA<= numOfChannels*2-1; loopA++)  
                 {
-                        printf("\n\nSensor number: %d, ",loopA);
-			printf("BitArray value: %d\n",bitArray[loopA]);
+                        printf("\n\nSensor number: %d\n",loopA);
+
+			printf("BEFORE: bitArray: %d, ",bitArray[loopA]);
+                        printf("bitArray+1:%d, ",bitArray[loopA+1]);
+			printf("bitArray-1:%d, ",bitArray[loopA-1]);
+                        printf(" inOutArray:%d, ",inOutArray[loopA]);
+                        printf(" inOutArray+1:%d, ",inOutArray[loopA+1]);
+			printf(" inOutArray-1:%d ",inOutArray[loopA-1]);
+                        printf("in: %d ",in);
+			printf("out: %d\n",out);
 
 
-                        /* Check for even number - bee is going from Inside -> Outside*/
+
+
+
+
+                       /* Check for even number - bee is going from Inside -> Outside*/
                         if(loopA % 2 == 0)
                                 {
-					printf("In even routiine\n");
-					printf("bitArray+1:%d ",bitArray[loopA+1]);
-					printf("inOutArray:%d ",inOutArray[loopA]);
-					printf("inOutArray+1:%d ",inOutArray[loopA+1]);
 
 					/* Case of bee walking from outside to inside, first sensor triggered */
 					if (bitArray[loopA] == 1 && bitArray[loopA+1] == 0 && inOutArray[loopA] == 0 && inOutArray[loopA+1] == 0)
@@ -136,32 +121,30 @@ int counter (int sensorNrIns, int bitArray[], long long timeInArray[])
 							inOutArray[loopA] = 1;
 							inOutArray[loopA+1] = 1;
 							timeArray[loopA] = getMicrotime();
+							printf("Inside EVEN pos 1\n");
 						}
 					/* Case of bee walking in and second sensor is triggeredm, time calculated */
-					else if (bitArray[loopA] == 1 && bitArray[loopA+1] == 1 && inOutArray[loopA] ==1)
+					else if (bitArray[loopA] == 1 && bitArray[loopA+1] == 1 && inOutArray[loopA] ==1 && timeArray[loopA+1] == 0)
 						{
 							timeDiff = getMicrotime() - timeArray[loopA];
 							timeInArray[timeInNumberArray] = timeDiff;
 							timeInNumberArray = timeInNumberArray+1;
 							in = in +1;
+							printf("Inside EVEN pos 2\n");
 						}
 
 					else if (bitArray[loopA] == 0 && bitArray[loopA+1] == 0 && inOutArray[loopA] == 1 && inOutArray[loopA+1] == 1)
 						{
 							inOutArray[loopA] = 0;
 							inOutArray[loopA+1] = 0;
-							printf("Opening channel, sensor nr %d",loopA);
+							printf("Inside EVEN pos 3\n");
 						}
+
 
                                 }
 
 			else
 				{
-					printf("In odd routiine\n");
-					printf ("Number: %d\n",loopA);
-                                        printf("bitArray-1:%d ",bitArray[loopA-1]);
-                                        printf("inOutArray:%d ",inOutArray[loopA]);
-                                        printf("inOutArray-1:%d ",inOutArray[loopA-1]);
 
 					/* Case of bee walking from outside to inside, first sensor triggered */
                                         if (bitArray[loopA] == 1 && bitArray[loopA-1] == 0 && inOutArray[loopA] == 0 && inOutArray[loopA-1] == 0)
@@ -169,30 +152,48 @@ int counter (int sensorNrIns, int bitArray[], long long timeInArray[])
                                                         inOutArray[loopA] = 1;
                                                         inOutArray[loopA-1] = 1;
                                                         timeArray[loopA] = getMicrotime();
+							printf("Inside ODD pos 1\n");
+
                                                 }
 					/* Case of bee walking in and second sensor is triggeredm, time calculated */
 
-                                        else if (bitArray[loopA] == 1 && bitArray[loopA-1] == 1 && inOutArray[loopA] ==1)
+                                        else if (bitArray[loopA] == 1 && bitArray[loopA-1] == 1 && inOutArray[loopA] ==1 && timeArray[loopA-1] == 0)
                                                 {
                                                         timeDiff = getMicrotime() - timeArray[loopA];
                                                         timeInArray[timeInNumberArray] = timeDiff;
                                                         timeInNumberArray = timeInNumberArray-1;
                                                         out = out +1;
+							printf("Inside ODD pos 2\n");
                                                 }
 
-                                        else if (bitArray[loopA] == 0 && bitArray[loopA+1] == 0 && inOutArray[loopA] == 1 && inOutArray[loopA-1] == 1)
+                                        else if (bitArray[loopA] == 0 && bitArray[loopA-1] == 0 && inOutArray[loopA] == 1 && inOutArray[loopA-1] == 1)
                                                 {
                                                         inOutArray[loopA] = 0;
                                                         inOutArray[loopA-1] = 0;
+							printf("Inside ODD pos 3\n");
                                                 }
 
 				}
 
+			printf("AFTER: bitArray: %d, ",bitArray[loopA]);
+                        printf("bitArray+1:%d, ",bitArray[loopA+1]);
+			printf("bitArray-1:%d, ",bitArray[loopA-1]);
+                        printf(" inOutArray:%d, ",inOutArray[loopA]);
+                        printf(" inOutArray+1:%d, ",inOutArray[loopA+1]);
+			printf("inOutArray-1:%d, ",inOutArray[loopA -1]);
+                        printf("in: %d ",in);
+			printf("out: %d",out);
+			printf("speedIN:%lld, ",timeInArray[loopA]);
+			printf("speedOUT:%lld, ",timeOutArray[loopA]);
+			printf("timeArray: %lld\n\n",timeArray[loopA]);
+
+
                 }
 
-	/*	printf("Inside function %d", bitArray[1]);*/
 		return 0;
 	}
+
+
 
 
 
